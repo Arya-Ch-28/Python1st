a797807538db93d43e91c38d7aa6d0eaf018d4e2
Arya-Ch-28 <chatautarya@gmail.com>
2025-05-17 21:51:09 +0545

    Create if_loop.py
    using if loop using imput

---
+++ if_loop.py
@@ -0,0 +1,10 @@
+choice = input("Enter a number from 1 to 3: ")
+
+if choice == "1":
+  print("You win Rs.200")
+elif choice == "2":
+  print("You win Rs.150")
+elif choice == "3":
+  print("You win Rs.400")
+else:
+  print("You have entered an incorrect choice. Run again")
